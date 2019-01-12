13eb75f12b992bb3245104a82adf61d38e311b8a
hezhen <hezhen112058@pwrd.com>
2019-01-12 16:40:55 +0800

    optimize code

--- mafengwo/client_crawler.py
+++ mafengwo/client_crawler.py
@@ -22,20 +22,20 @@ class arguments:
 
 def parse_app_arguments():
     parser = argparse.ArgumentParser(prog='CrawlerClient', description='Start a crawler client')
-    parser.add_argument('-S', '--host-all', type=str, nargs=1, help='Host server for all services')
-    parser.add_argument('-s', '--host', type=str, nargs=1, help='Crawler host server address, default is localhost')
+    parser.add_argument('-h', '--host', type=str, nargs=1, help='Crawler host server address, default is localhost')
     parser.add_argument('-p', '--host-port', type=int, nargs=1, help='Crawler host server port number, default is 20100')
     parser.add_argument('-m', '--mongo', type=str, nargs=1, help='Mongo Server address, default is localhost')
     parser.add_argument('-n', '--mongo-port', type=int, nargs=1, help='Mongo port number, default is 27017')
     parser.add_argument('-r', '--redis', type=str, nargs=1, help='Redis server address, default is localhost')
     parser.add_argument('-x', '--redis-port', type=int, nargs=1, help='Redis port number, default is 6379')
+    parser.add_argument('-s', '--server', type=str, nargs=1, help='Server address for all services, including mongo, redis and spider')
 
     args = arguments()
 
     parser.parse_args(namespace=args)
 
-    if args.host_all is not None:
-        args.host = args.mongo = args.redis = args.host_all
+    if args.server is not None:
+        args.host = args.mongo = args.redis = args.server
 
     if args.host is None:
         args.host = 'localhost'
